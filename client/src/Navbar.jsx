bf5f616624b5b6df69528abc1140b2ef3def81d8
Boris <b2kizenko@gmail.com>
2023-09-16 12:03:12 -0400

    added navbar and styles

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar () {
+    return <nav className="nav">
+        <a href="/" className = "site-title"> Site Name</a>
+        <ul> 
+            <li className="active"> 
+                <a href="/pricing"> Pricing </a>
+                < li> 
+
+                </li>
+                <a href="/about"> About </a>
+            </li>
+        </ul>
+    </nav>
+}
