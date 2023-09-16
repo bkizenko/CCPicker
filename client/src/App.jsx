f7ca8acbff97d6ca29ab7484e9f04f4d24db720a
Boris <b2kizenko@gmail.com>
2023-09-16 16:48:24 -0400

    added some buttons and questions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function App() {
   const [creditScore, setCreditScore] = useState('');
   const [income, setIncome] = useState('');
+  const [ccFirst, setCCFirst] = useState('');
 
   const handleChange = (e) => {
     setCreditScore(e.target.value);
@@ -13,6 +14,24 @@ function App() {
       setIncome(e.target.value);
     }
   };
+
+  const handleChange3 = (e) => {
+    if (income != ' ') {
+      setCCFirst(e.target.value);
+    }
+  };
+
+  const saveCreditScore = () => {
+    console.log('Credit Score Saved', creditScore);
+  }
+
+  const saveIncome = () => {
+    console.log('Income Saved', income);
+  }
+
+  const saveccFirst = () => {
+    console.log('CCFirst Saved', ccFirst);
+  }
   return (
     <>
       <h1> Credit Card Picker </h1>
@@ -25,6 +44,7 @@ function App() {
           onChange={handleChange}
           value={creditScore}
         />
+        < button onClick = {saveCreditScore}> Save </button>
         <p> What is your income? </p>
         < input 
           type="text"
@@ -32,8 +52,8 @@ function App() {
           id="income"
           onChange={handleChange2}
           value={income}
-
         /> 
+        < button onClick = {saveIncome}> Save </button>
       </div>
       <div className="cscorea">
         Your credit score is: {creditScore}
@@ -41,8 +61,27 @@ function App() {
       <div className="incomeAnswer">
         Your income  is: {income}
       </div>
+      < p className="ccfirstq"> Is this your first credit card? (Type 'Yes' or 'No') </p>
+      <div className="ccfirsta">
+      <input
+          type="text"
+          name="ccFirst"
+          id="ccFirst"
+          onChange={handleChange3}
+          value={ccFirst}
+        />
+      < button onClick = {saveccFirst}> Save </button>
+      </div>
+      <p className = "ccFirstanswer">This if your first CC: {ccFirst}  </p> 
+
     </>
   );
 }
 
+
 export default App;
+
+
+// first credit card?
+//  preferences: travel, groceries, gas
+// idk some other one
