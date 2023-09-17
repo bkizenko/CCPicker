67496812d2ce870db1ccca6122359f83f3e85d0c
Boris <b2kizenko@gmail.com>
2023-09-17 02:09:30 -0400

    navbar and stuff attept 2

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return <nav className="nav">
+        < a href="/" className="site-title"> Credit Card Matcher </a>
+        <ul className="active">
+            <li className="nav-item">
+            </li>
+        </ul>
+    </nav>
+}
+// when they are on top of each other the elements they get stacked
